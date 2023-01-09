4941d12ee16e5d577fc16b05755153869e95f8a6
xdpiqbx <xdpiqbx@gmail.com>
2023-01-09 22:08:03 +0200

    fix DefaultSecurityConfig

--- src/main/java/cw/sprboot/dpiqb/feature/auth/DefaultSecurityConfig.java
+++ src/main/java/cw/sprboot/dpiqb/feature/auth/DefaultSecurityConfig.java
@@ -3,19 +3,16 @@ package cw.sprboot.dpiqb.feature.auth;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-@Component
+@Service
 public class DefaultSecurityConfig {
   private final CustomAuthProvider authProvider;
 //  @Bean
